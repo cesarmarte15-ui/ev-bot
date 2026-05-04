0d276e0c379b2801064b5fdfb816bdec3ba394cd
cesarmarte15-ui <cesarmarte15@gmail.com>
2026-05-03 21:34:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,6 @@ def api_picks():
 
 import os
 
-port = int(os.environ.get("PORT", 10000))
-
-app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
